afbe3d4e7a2650c10053953842d020fe629c1662
pirasp382 <pirasp382@gmail.com>
2024-07-14 20:31:21 +0200

    all commits

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("how are you today?");
+        System.out.println(maths.add(2, 3));
     }
 }
---
+++ src/main/java/com/example/maths.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class maths {
+    
+
+    public static int add(final int a, final int b){
+        return a+b;
+    }
+}
