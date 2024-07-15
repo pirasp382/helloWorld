402d58da511b3679f706159e67688efa48219d70
pirasp382 <pirasp382@gmail.com>
2024-07-15 05:42:35 +0200

    add test

--- src/test/java/MathsTest.java
+++ src/test/java/MathsTest.java
@@ -1,3 +1,16 @@
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import com.example.maths;
+
 public class MathsTest {
     
+    @Test
+    void testen(){
+        int a=2;
+        int b = 3;
+        int expected = 5;
+        int output = maths.add(a, b);
+        Assertions.assertThat(output).isEqualTo(expected);
+    }
 }
